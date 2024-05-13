f40f68c2473e6ad07f27f347836600605f98526b
Kabir1983 <humaiunkabir.abfl@gmail.com>
2024-05-13 10:20:00 +0600

    Done.
    Done

--- EmployeeManagementSystem/EMS/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ EmployeeManagementSystem/EMS/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a50a13c935c1e6c3b7624e00eb943a5a6efd5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EmployeeManagementSystem/EMS/ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
+++ EmployeeManagementSystem/EMS/ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a50a13c935c1e6c3b7624e00eb943a5a6efd5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
